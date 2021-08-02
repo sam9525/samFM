7d2f966c0b517bb00bdac09b735174c299ac956d
sam <sam9985246327@gmail.com>
2021-08-02 15:33:04 +0800

    0802

--- js/all.js
+++ js/all.js
@@ -7,6 +7,9 @@ $(document).ready(function () {
 		}if ($(event.target).hasClass("icon-add")) {
 			$(event.target).parent(".nav-title").parent(".nav-box").toggleClass("active");
 			$(event.target).parent(".nav-title").siblings(".nav-content").slideToggle("300");
+		}
+		if($(event.target).hasClass("nav_link")) {
+			$(event.target).parent(".nav_item").toggleClass("active");
 		}		
 		if($(event.target).hasClass("para")){
 			$(event.target).siblings(".like").toggleClass("active");
